235b698e2bf478ad0a56cf249b04eaedd96ae277
Lillard <halila@me.com>
2018-11-05 19:05:34 +0800

    新增Object to Json的擴充方法

--- CTExtensions.TestConsole/Program.cs
+++ CTExtensions.TestConsole/Program.cs
@@ -11,40 +11,62 @@ namespace CTExtensions.TestConsole
         {
             Console.WriteLine("Hello CTExtensions!");
 
-            JObject json = new JObject();
-            json.AddProperty("Prop1", "str1");
-            json.AddProperty("Prop2", DateTime.Now);
-            json.AddProperty("Prop3", 3);
-            json.AddProperty("Prop4", Guid.NewGuid());
-            json.AddProperty("Prop5", null);
+            //JObject json = new JObject();
+            //json.AddProperty("Prop1", "str1");
+            //json.AddProperty("Prop2", DateTime.Now);
+            //json.AddProperty("Prop3", 3);
+            //json.AddProperty("Prop4", Guid.NewGuid());
+            //json.AddProperty("Prop5", null);
 
 
-            var str1 = json.GetProperty<string>("Prop1");
-            var str2 = json.GetProperty<string>("Prop2", true);
+            //var str1 = json.GetProperty<string>("Prop1");
+            //var str2 = json.GetProperty<string>("Prop2", true);
 
 
-            Console.WriteLine($"{str1}  {str2}");
+            //Console.WriteLine($"{str1}  {str2}");
 
 
-            if (json.GetProperty<string>("Prop1") == str1)
+            //if (json.GetProperty<string>("Prop1") == str1)
+            //{
+            //    Console.WriteLine($"Prop1屬性還在");
+            //}
+
+            //if (json.GetProperty<string>("Prop2") == null)
+            //{
+            //    Console.WriteLine($"Prop2屬性已移除");
+            //}
+
+            //var int3 = json.GetProperty<int>("Prop3");
+            //Console.WriteLine(int3);
+
+            //var dat4 = json.GetProperty<Guid>("Prop4");
+            //Console.WriteLine(dat4);
+
+            var test = new Test()
             {
-                Console.WriteLine($"Prop1屬性還在");
-            }
+                MyProperty4 = new List<string>
+                {
+                    "1","2","3"
+                },
+                MyProperty5 = new JObject
+                {
 
-            if (json.GetProperty<string>("Prop2") == null)
-            {
-                Console.WriteLine($"Prop2屬性已移除");
-            }
+                }
+            };
 
-            var int3 = json.GetProperty<int>("Prop3");
-            Console.WriteLine(int3);
-
-            var dat4 = json.GetProperty<Guid>("Prop4");
-            Console.WriteLine(dat4);
-
-
+            Console.WriteLine(test.ToJson());
 
             Console.ReadKey();
         }
+    }
+
+    public class Test
+    {
+        public int MyProperty1 { get; set; }
+        public string MyProperty2 { get; set; }
+        public bool MyProperty3 { get; set; }
+        public List<string> MyProperty4 { get; set; }
+        public JObject MyProperty5 { get; set; }
+        public JObject[] MyProperty6 { get; set; }
     }
 }
---
+++ CTExtensions/ObjectExtension.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CTExtensions
+{
+    public static class ObjectExtension
+    {
+        public static string ToJson(this object obj)
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(obj);
+                return json;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
